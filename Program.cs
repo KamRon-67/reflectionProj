3d997721f5fca0323d6082b428d7d77bef73bf00
Cameron Young <Cameronslatton@gmail.com>
2021-04-21 22:55:51 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Reflection;
+using System.Linq;
 
 namespace reflectionProj
 {
-     class Program
-    {
-        static void Main(string[] args)
+	public class Program
+	{
+		public static void Main()
 		{
 			Type personType = typeof(Person);
 
@@ -14,11 +16,19 @@ namespace reflectionProj
 				Console.WriteLine($"Property: Type: {item.PropertyType.Name} | Name: {item.Name}");
 			}
 
+			Person person = new Person();
+
 			var methods = personType.GetMethods();
 			foreach (var item in methods)
 			{
 				Console.WriteLine($"Method: Type: {item.ReturnType.Name} | Name {item.Name}");
+				if (item.Name == "Print")
+				{
+					item.Invoke(person, null);
+				}
 			}
+			Console.WriteLine("-------------------------------");
+			AttributeTest(typeof(Person));
 
 			Console.ReadKey();
 		}
@@ -30,9 +40,16 @@ namespace reflectionProj
 			public string Phone { get; set; }
 			public int ZipCode { get; set; }
 
+			[MethodForRun(RunCount = 3)]
 			public void Print()
 			{
 				Console.WriteLine($"{FirstName} {LastName}");
+			}
+
+			[MethodForRun(RunCount = 3)]
+			public void TestMethod()
+			{
+				Console.WriteLine("Hello from TestMethod");
 			}
 
 			public void Move(int newZipCode)
@@ -40,6 +57,37 @@ namespace reflectionProj
 				ZipCode = newZipCode;
 				Console.WriteLine($"{FirstName} {LastName} has been moved to {newZipCode}");
 			}
+
+			[MethodForRun(RunCount = 1)]
+			public void SayHi()
+			{
+				Console.WriteLine($"Hi {FirstName}");
+			}
+		}
+
+
+		static void AttributeTest(Type type)
+		{
+			// Get the methods
+			var allMethods = type.GetMethods();
+			var methodsWithAttribute = allMethods.Where(m => m.GetCustomAttribute(typeof(MethodForRunAttribute)) != null);
+
+			var obj = Activator.CreateInstance(type);
+
+			foreach (var item in methodsWithAttribute)
+			{
+				var attribute = (MethodForRunAttribute)item.GetCustomAttribute(typeof(MethodForRunAttribute));
+				Console.WriteLine($"{item.Name} run for {attribute.RunCount} times");
+				for (int i = 0; i < attribute.RunCount; i++)
+				{
+					item.Invoke(obj, null);
+				}
+			}
+		}
+
+		public class MethodForRunAttribute : Attribute
+		{
+			public int RunCount { get; set; }
 		}
 	}
 }
